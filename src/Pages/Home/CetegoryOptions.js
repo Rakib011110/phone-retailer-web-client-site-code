ff1de246e4e93e8e1cbcf613a076d1856c507aae
Rakib011110 <rakib011110@gmail.com>
2022-11-28 22:31:13 +0600

    category option add

--- src/Pages/Home/CetegoryOptions.js
+++ src/Pages/Home/CetegoryOptions.js
@@ -13,9 +13,9 @@ const CetegoryOptions = ({ category }) => {
                 </figure>
                 <div className="card-body items-center text-center">
                     <h2 className="card-title">{category.name} </h2>
-                    <div className="card-actions justify-end">
+                    {/* <div className="card-actions justify-end">
                         <Link to={`/products/${category._id}`}> <button className="btn btn-primary btn-sm">Buy Now</button></Link>
-                    </div>
+                    </div> */}
                 </div>
             </div>
 
